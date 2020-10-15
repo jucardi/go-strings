988ea872be17187149b7e9e52fea5703421a19b6
Juan C. Diaz <me@jucardi.com>
2020-10-14 18:06:14 -0700

    Properly converting to camelCase. Added PascalCase conversion

--- stringx/conversion.go
+++ stringx/conversion.go
@@ -12,14 +12,23 @@ func CamelToSnake(s string) string {
 	return camelToSymbolSeparated(s, "_")
 }
 
-// SnakeToCamel converts snake_case to CamelCase
-func SnakeToCamel(s string) string {
-	return symbolSeparatedToCamel(s, "_")
-}
-
 // CamelToDash converts CamelCase to dash-separated-string
 func CamelToDash(s string) string {
 	return camelToSymbolSeparated(s, "-")
+}
+
+// CamelToSnake converts CamelCase to snake_case.
+func PascalToSnake(s string) string {
+	return camelToSymbolSeparated(s, "_")
+}
+
+func PascalToDash(s string) string {
+	return camelToSymbolSeparated(s, "-")
+}
+
+// SnakeToCamel converts snake_case to CamelCase
+func SnakeToCamel(s string) string {
+	return symbolSeparatedToCamel(s, "_")
 }
 
 // DashToCamel converts a dash-separated-string to CamelCase
@@ -27,14 +36,29 @@ func DashToCamel(s string) string {
 	return symbolSeparatedToCamel(s, "-")
 }
 
-func symbolSeparatedToCamel(s string, separator string) string {
-	var ret []string
+// SnakeToCamel converts snake_case to CamelCase
+func SnakeToPascal(s string) string {
+	return symbolSeparatedToPascal(s, "_")
+}
+
+// DashToCamel converts a dash-separated-string to CamelCase
+func DashToPascal(s string) string {
+	return symbolSeparatedToPascal(s, "-")
+}
+
+func symbolSeparatedToPascal(s string, separator string) string {
+	var ret string
 
 	for _, v := range strings.Split(s, separator) {
-		ret = append(ret, strings.Title(v))
+		ret += strings.Title(v)
 	}
 
-	return strings.Join(ret, "")
+	return ret
+}
+
+func symbolSeparatedToCamel(s string, separator string) string {
+	str := symbolSeparatedToPascal(s, separator)
+	return strings.ToLower(str[:1]) + str[1:]
 }
 
 func camelToSymbolSeparated(s string, separator string) string {
--- stringx/conversion_test.go
+++ stringx/conversion_test.go
@@ -7,21 +7,41 @@ import (
 )
 
 func TestCamelToDash(t *testing.T) {
-	converted := CamelToDash("SomethingCamelCase")
+	converted := CamelToDash("somethingCamelCase")
 	assert.Equal(t, "something-camel-case", converted, "Conversion mismatch")
+}
+
+func TestCamelToSnake(t *testing.T) {
+	converted := CamelToSnake("somethingCamelCase")
+	assert.Equal(t, "something_camel_case", converted, "Conversion mismatch")
 }
 
 func TestDashToCamel(t *testing.T) {
 	converted := DashToCamel("something-dash-separated")
-	assert.Equal(t, "SomethingDashSeparated", converted, "Conversion mismatch")
-}
-
-func TestCamelToSnake(t *testing.T) {
-	converted := CamelToSnake("SomethingCamelCase")
-	assert.Equal(t, "something_camel_case", converted, "Conversion mismatch")
+	assert.Equal(t, "somethingDashSeparated", converted, "Conversion mismatch")
 }
 
 func TestSnakeToCamel(t *testing.T) {
 	converted := SnakeToCamel("something_snake_case")
+	assert.Equal(t, "somethingSnakeCase", converted, "Conversion mismatch")
+}
+
+func TestPascalToDash(t *testing.T) {
+	converted := CamelToDash("SomethingPascalCase")
+	assert.Equal(t, "something-pascal-case", converted, "Conversion mismatch")
+}
+
+func TestPascalToSnake(t *testing.T) {
+	converted := CamelToSnake("SomethingPascalCase")
+	assert.Equal(t, "something_pascal_case", converted, "Conversion mismatch")
+}
+
+func TestDashToPascal(t *testing.T) {
+	converted := DashToPascal("something-dash-separated")
+	assert.Equal(t, "SomethingDashSeparated", converted, "Conversion mismatch")
+}
+
+func TestSnakeToPascal(t *testing.T) {
+	converted := SnakeToPascal("something_snake_case")
 	assert.Equal(t, "SomethingSnakeCase", converted, "Conversion mismatch")
 }
